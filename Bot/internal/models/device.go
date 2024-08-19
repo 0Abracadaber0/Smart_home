4fa41501c2942c8b581b983370a84142f4eef5c1
Yauheni <yauhenbelarus@gmail.com>
2024-08-19 18:01:17 +0300

    Added Device model

--- Bot/internal/models/device.go
+++ Bot/internal/models/device.go
@@ -1,1 +1,18 @@
 package model
+
+type Device struct {
+	NwkAddr       string `json:"nwkAddr"`
+	IeeeAddr      string `json:"ieeeAddr"`
+	Flags         int32  `json:"flags"`
+	Friendly_name string `json:"friendly_name"`
+	State         St     `json:"st"`
+}
+
+type St struct {
+	State       string  `json:"state"`
+	LastSeen    int32   `json:"last_seen"`
+	Battery     int     `json:"battery"`
+	Gas         string  `json:"gas"`
+	Temperature float32 `json:"temperature"`
+	Contact     string  `json:"contact"`
+}
